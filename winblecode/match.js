04cf6a372009496a28fb0d890349dbd2e01af7a1
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-17 14:23:55 +0200

    feat: add win condition inside addScore fn

--- winblecode/match.js
+++ winblecode/match.js
@@ -2,19 +2,31 @@ const player1 = {
   id: 1,
   name: "",
   score: 0,
+  roundsWon: 0,
 };
 
 const player2 = {
   id: 2,
   name: "",
   score: 0,
+  roundsWon: 0,
 };
 
 export default function createMatch(player1Name, player2Name) {
   player1.name = player1Name;
   player2.name = player2Name;
 
+  const resetRound = () => {
+    player1.score = 0;
+    player2.score = 0;
+  };
+
   const addScore = (player) => {
+    if (player.score === 40) {
+      player.roundsWon += 1;
+      resetRound();
+    }
+
     player.score < 30 ? (player.score += 15) : (player.score += 10);
   };
 
