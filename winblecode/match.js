29b40239d061b1cd17cd4ac5b938f4dcafa53c7f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-17 14:11:45 +0200

    feat: add required functions inside createMatch and return them

--- winblecode/match.js
+++ winblecode/match.js
@@ -13,4 +13,22 @@ player2 = {
 export const createMatch = (player1Name, player2Name) => {
   player1.name = player1Name;
   player2.name = player2Name;
+
+  const pointWonBy = (id) => {};
+
+  const getCurrentRoundsScore = () => {};
+
+  const getGameScore = () => {};
+
+  const getMatchScore = () => {};
+
+  const getWinner = () => {};
+
+  return {
+    pointWonBy,
+    getCurrentRoundsScore,
+    getGameScore,
+    getMatchScore,
+    getWinner,
+  };
 };
