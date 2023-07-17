d0fcabe153422522216344a89cd2d18b98fd1ef5
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-17 23:32:51 +0200

    fix: fix bug using a Promise

--- bugAsync.js
+++ bugAsync.js
@@ -10,3 +10,16 @@ function obtenerUsuario(id) {
 
 const usuario = obtenerUsuario(1);
 console.log(usuario);
+
+function obtenerUsuarioPromise(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (id === 1) {
+        resolve({ id: 1, nombre: "John Doe" });
+      }
+    }, 2000);
+  });
+}
+
+const usuarioPromise = await obtenerUsuarioPromise(1);
+console.log(usuarioPromise);
