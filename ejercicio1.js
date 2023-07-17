5cbf167b80527a3e5d89cf41e4d83d5c5b612501
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-17 23:20:10 +0200

    feat: exercise one done!

---
+++ ejercicio1.js
@@ -0,0 +1,14 @@
+const alumno = {
+  name: "Jose Alberto Delgado Robles",
+  bootcamKnowledge: ["html", "css", "javascript", "react", "node.js"],
+  socialMedia: [
+    {
+      name: "github",
+      link: "https://github.com/JoseAlbDR",
+    },
+    {
+      name: "linkedin",
+      link: "www.linkedin.com/in/jalbertodelgado",
+    },
+  ],
+};
