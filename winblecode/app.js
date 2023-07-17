57ab9cf58f6ca51409fe7310d2d82b65c0e3621b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-17 14:19:46 +0200

    feat: creat addScore function to handle score increase in players

--- winblecode/app.js
+++ winblecode/app.js
@@ -1,4 +1,7 @@
 import createMatch from "./match.js";
 
 const game = createMatch("Alberto C", "David J");
+
+game.pointWonBy(1);
+game.pointWonBy(2);
 console.log(game.getCurrentRoundScore());
--- winblecode/match.js
+++ winblecode/match.js
@@ -14,7 +14,13 @@ export default function createMatch(player1Name, player2Name) {
   player1.name = player1Name;
   player2.name = player2Name;
 
-  const pointWonBy = (id) => {};
+  const addScore = (player) => {
+    player.score < 30 ? (player.score += 15) : (player.score += 10);
+  };
+
+  const pointWonBy = (id) => {
+    id === 1 ? addScore(player1) : addScore(player2);
+  };
 
   const getCurrentRoundScore = () => {
     return `${player1.name} ${player1.score} - ${player2.score} ${player2.name}`;
