54a843699d66bf66aebadc70a9dee6e056213040
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-17 19:49:03 +0200

    Create groupA and groupB with 2 random players each Create empty groupC Print Group A and Group B members Create Match for Group A Show Winner of Group A and push him to Group C Create Match for Group B Show Winner of Group b and push him to Group C Create Match for Group C Show Final Winner

--- winblecode/game.js
+++ winblecode/game.js
@@ -1,9 +1,16 @@
+import createMatch from "./match.js";
+import { winner } from "./match.js";
+
 const players = [
   "Alberto Casero",
   "David Jiménez",
   "Javier de Miguel",
   "Eduardo Aguilar",
 ];
+
+const randomPoint = function () {
+  return Math.floor(Math.random() * 2) + 1;
+};
 
 const getRandomPlayers = function (players) {
   const shufled = [...players].sort(() => Math.random() - 0.5);
@@ -14,4 +21,36 @@ const getRandomPlayers = function (players) {
 export default function gameLoop() {
   const groupA = getRandomPlayers(players);
   const groupB = players.filter((player) => !groupA.includes(player));
+  const groupC = [];
+
+  console.log("Group A: ", groupA);
+  console.log("Group B: ", groupB);
+
+  const game1 = createMatch(...groupA);
+
+  while (winner === "") {
+    game1.pointWonBy(randomPoint());
+    game1.getWinner();
+  }
+  const groupAWinner = winner;
+  console.log("Group A Winner: ", groupAWinner);
+  groupC.push(groupAWinner);
+
+  const game2 = createMatch(...groupB);
+  while (winner === "") {
+    game2.pointWonBy(randomPoint());
+    game2.getWinner();
+  }
+  const groupBWinner = winner;
+  console.log("Group B Winner: ", groupBWinner);
+  groupC.push(groupBWinner);
+
+  console.log("Group C: ", groupC);
+  const finalGame = createMatch(...groupC);
+  while (winner === "") {
+    finalGame.pointWonBy(randomPoint());
+    finalGame.getWinner();
+  }
+
+  console.log("Final Winner: ", winner);
 }
